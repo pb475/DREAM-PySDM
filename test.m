4ec574bd771ac00247b6ffe19a7280067f339708
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-01-13 13:16:53 -0600

    computing kappa from volfracs

--- test.m
+++ test.m
@@ -26,47 +26,41 @@ Pars(1,4) = 0.9000 %Soluble volume fraction (soluble component = (nh4)hso4, inso
 
 format='%10.4e'
   
-%In first comparison have two modes, internally mixed (same chemistry in
-%each). In future tests we should allow chemistry to vary per mode. For
-%now, lets keep as is and ensure icpm vs drops similar. 
+%Compute effective kappa
+density_sol   = 1770;       %Kg/m3 (nh4)2so4
+density_insol = 2000;       %Kg/m3 BC
+molar_mass_sol = 0.13216;   %(nh4)2so4
+ionic_dissociation_sol = 3; %(nh4)2so4
+rho_water = 1000;
+Mv_water  = 0.018;
 
-% Compute effective kappa. 
+volfrac_sol = Pars(1,7);
+volfrac_insol = 1- Pars(1,7);
+ns_sol = (ionic_dissociation_sol * 1 / (molar_mass_sol/density_sol));
+ns = ns_sol;
+kappa_sol = ns * Mv_water / rho_water;
+kappa = kappa_sol * volfrac_sol;
   
-  %In the previous version of Drops it was not
-  % possible to initialise the model with volume fractions (as done in icpm
-  % parcel model). This was a workaround to initialise a soluble volume
-  % fraction [soluble component represented by (nh4)hso4, insoluble component BC ]
-  kpa_s = .61;
-  d_s = 1.77;
-  d_i = 2.10;
-  
-  %In ICPM the following paramter values are used
-  %Mass accomodation coefficient = 1; 
-  %Surface tension water = 72.8;
-  
-  %Write the parameters to a structure (all in SI units)
-  dt = .25;                      %Timestep
-  input = struct(...
+% Write the parameters to a structure (all in SI units)
+dt = .25;                      %Timestep
+input = struct(...
     'n_bins', 250, ...           %Number of bins
     'T',  Pars(1,9), ...         %Initial Temperature (K)
     'RH', .999, ...              %Initial Relative Humidity (%)
     'p',  1.00e+05, ...          %Initial Pressure (Pa)
     'w',  Pars(1,5), ...         %Updraft velocity (m/s)
-    'kappa', .5, ... %kpa_s * volfrac(Pars(1,4), d_s, d_i), ...
+    'kappa', kappa, ... 
     'n_tot', [num2str(Pars(1,1),format),        ' ', num2str(Pars(1,6),format)       ], ...  %N mode 1,2
     'meanr', [num2str(Pars(1,2) * 1e-6,format), ' ', num2str(Pars(1,7) * 1e-6,format)], ...  %Mean radius mode 1,2
     'gstdv', [num2str(Pars(1,3),format),        ' ', num2str(Pars(1,8),format)       ], ...  %GSD mode 1,2
     'dt', dt, ... 
     'nt', ceil(1000 / Pars(1,5) / dt) ... % TODO: stop at S_max !                            %Output height. Needs changing so stop at S_max. 
-  );  
+);  
 
     
-  % Run the forward model 
-  %try
-    output = drops_py(input);
-  %catch
-  %end
+% Run the forward model 
+output = drops_py(input);
    
-  % Define the return vector for comparison with the measurements
-  N_Parcel = [output.N_act, output.S_max]; 
-  N_Parcel = N_Parcel(:);
+% Define the return vector for comparison with the measurements
+N_Parcel = [output.N_act, output.S_max]; 
+N_Parcel = N_Parcel(:);
